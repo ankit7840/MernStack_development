f4ca46ea392a5d0416e99f9018875fcd0f8365c9
ankit kumar <87237698+ankit7840@users.noreply.github.com>
2023-12-28 17:27:30 +0000

    4th day of react series

--- react /react_with_vite /react_router/src/App.jsx
+++ react /react_with_vite /react_router/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
  
 
   return (
     <>
-        <h1 className="text-3xl font-bold underline bg-slate-600">
-      Hello world!
-    </h1>
+      
     </>
   )
 }
