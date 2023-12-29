962d99dac52cd4e3568d9ae44ee421bc22c28ed1
ankit kumar <87237698+ankit7840@users.noreply.github.com>
2023-12-29 16:41:36 +0000

    5th day of react

--- react /react_with_vite /react_router/src/Layout.jsx
+++ react /react_with_vite /react_router/src/Layout.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
-
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-   <>
-     <Header/>
-      <Outlet />
-     <Footer/>
-   </>
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
   )
 }
 
-export default Layout
+export default Layout;
